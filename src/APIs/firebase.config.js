61fa8d6fc97233097fee9833ddb0fdfbc6e3a17b
MarcoGrimaldo <marco.a.grimaldo98@gmail.com>
2021-08-27 10:40:56 -0500

    firebase conection

---
+++ src/APIs/firebase.config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAsT_tVzKoe5G3Qqp6IsrRuMXkuXgg7Baw",
+    authDomain: "froggy-4cd76.firebaseapp.com",
+    databaseURL: "https://froggy-4cd76-default-rtdb.firebaseio.com",
+    projectId: "froggy-4cd76",
+    storageBucket: "froggy-4cd76.appspot.com",
+    messagingSenderId: "627644063678",
+    appId: "1:627644063678:web:8c53441acd4666156a63ef"
+  };
+  
+initializeApp(firebaseConfig);
+
+const db = getFirestore();
+
+export default db;
--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import db from './APIs/firebase.config';
+import React,{useEffect} from 'react';
+import {  doc, getDoc, collection, query, where } from "firebase/firestore";
 
 function App() {
+
+  const fetchBlogs = async() => {
+
+    const docRef = doc(db, "froggy-4cd76-default-rtdb", "data");
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      console.log("Document data:", docSnap.data());
+    } else {
+      // doc.data() will be undefined in this case
+      console.log("No such document!");
+    }
+
+    const citiesRef = collection(db, "cities");
+
+    // Create a query against the collection.
+    const q = query(citiesRef, where("state", "==", "CA"));
+    console.log(q)
+
+    //const dataRef = collection(db,"froggy-4cd76-default-rtdb");
+    //console.log(dataRef);
+    //const data = await setDoc(doc())
+    //const data=await response.get();
+    //data.docs.forEach(item=>{
+    // setBlogs(data.data());
+    //}
+  }
+  useEffect(() => {
+    fetchBlogs();
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
