adc547677e78ff5621c86dd28ebe5cea600498d5
Marco Grimaldo <voskogrim@gmail.com>
2021-12-05 23:25:17 -0600

    ph ajuste

--- src/Monitor.js
+++ src/Monitor.js
@@ -56,7 +56,7 @@ const Monitor = (state) => {
   //Split the data "xx.xx , xx.xx, xx.xx"
   const getDataString = (dataString) => {
     var arrayData = dataString.split(',');
-    setdataPh(arrayData[0]);
+    setdataPh(arrayData[0]+2);
     setdataTemp(arrayData[1]);
     setdataHr(arrayData[2]);
     
